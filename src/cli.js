4737c6d1f223cf197d7b8e3127f299516e7242d1
Anastasiia <bjko.smsng@gmail.com>
2022-12-01 03:59:03 +0300

    add .eslint.yml

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
+/* eslint-disable linebreak-style */
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-console.log('Welcome to the Brain Games!');
-let userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default greeting;
